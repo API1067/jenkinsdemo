8df7197f006694f22b0d81f79a69a2a488eb4a2a
Sahil Sinha <sahil.sinha@apisero.com>
2021-04-08 15:30:14 +0530

    First commit

---
+++ wel.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class wel{
+public static void main(String args[])
+{
+System.out.println("WELCOME!!!");
+System.out.println("New addition");
+  System.out.println("latest");
+System.out.println(" branch test");
+  System.out.println("Fourth");
+System.out.println("Jenkins test");
+System.out.println("Variable test via jenkins");
+System.out.println("Variable test via jenkins2");
+}
+}
